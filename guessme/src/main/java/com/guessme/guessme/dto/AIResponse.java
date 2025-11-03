17fb16feca7261bc106391cecccb6737e165e60b
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-03 16:58:58 -0300

    Add dto

---
+++ guessme/src/main/java/com/guessme/guessme/dto/AIResponse.java
@@ -0,0 +1,19 @@
+package com.guessme.guessme.dto;
+
+public class AIResponse {
+    private String response;
+
+    public AIResponse() {}
+
+    public AIResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
