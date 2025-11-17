0b14d755e1e5cbf1f9ddcbe2946a7e92515f700b
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-17 10:06:54 -0300

    Adjustments

--- guessme/src/main/java/com/guessme/guessme/service/GameService.java
+++ guessme/src/main/java/com/guessme/guessme/service/GameService.java
@@ -16,7 +16,6 @@ import java.util.Map;
 public class GameService {
 
     private final GeminiConfig geminiConfig;
-
     private final WebClient geminiWebClient;
 
     public Mono<AIResponse> askAI(String question) {
