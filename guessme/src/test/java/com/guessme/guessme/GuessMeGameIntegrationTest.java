8c8ac90557c9919a502dc32d86edc69dcde828f9
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-25 20:16:14 -0300

    Adjustments

--- guessme/src/test/java/com/guessme/guessme/GuessMeGameIntegrationTest.java
+++ guessme/src/test/java/com/guessme/guessme/GuessMeGameIntegrationTest.java
@@ -6,7 +6,6 @@ import com.guessme.guessme.service.GameService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Mono;
 
 @SpringBootTest
 public class GuessMeGameIntegrationTest {
@@ -18,17 +17,18 @@ public class GuessMeGameIntegrationTest {
     void testMotokoVictory() {
 
         AIResponse startResponse = gameService.startGame().block();
-        System.out.println("IA inicial: " + startResponse.text());
+        System.out.println("IA inicial: " + startResponse.answer());
+
         AIResponse aiResponse = gameService.askAI(
                 "O personagem é Motoko Kusanagi, de Ghost in the Shell?"
         ).block();
 
-        System.out.println("Resposta IA: " + aiResponse.text());
-        if (aiResponse.vitoria()) {
+        System.out.println("Resposta IA: " + aiResponse.answer());
+        if (aiResponse.success()) {
             CharacterData c = aiResponse.character();
             System.out.println("=== RESPOSTA DETECTADA ===");
             System.out.println("Nome: " + c.name());
-            System.out.println("Obra: " + c.origin());
+            System.out.println("Obra: " + c.work());
             System.out.println("Imagem: " + c.image());
         }
     }
